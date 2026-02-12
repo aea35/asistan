648a8f38dddf98bd1c4deb81c4c0b391c4e60ed4
aea35 <ikimsn@hotmail.com>
2026-02-12 16:16:30 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,78 @@
+// sw.js dosyasının tamamı:
+
+const CACHE_NAME = 'alptekin-pro-v1'; // Versiyonu değiştirdik ki tarayıcı yenilesin
+
+const OFFLINE_DOSYALAR = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  './images.png',
+  './logo.png',
+  './categories.js',
+  './dictionary.js',
+  './hikayedata.js',
+  './categories.js',
+  './data.js',
+  './chess.html',
+  './sudoku.html',
+  './bulmaca.html',
+  './libs/worker.min.js',
+  './libs/tesseract.min.js',
+  './libs/tesseract-core.wasm',
+  './libs/tesseract-core.wasm.js',
+  './libs/cropper.min.css',
+  './libs/cropper.min.js',
+  './libs/tur.traineddata.gz',
+  './assets/logo.png',
+  // SATRANÇ TAŞLARI VE SESLERİ (Eksik olan kısım burasıydı)
+  './assets/imaj/B.svg', './assets/imaj/K.svg', './assets/imaj/N.svg', './assets/imaj/P.svg', './assets/imaj/Q.svg', './assets/imaj/R.svg',
+  './assets/imaj/b1.svg', './assets/imaj/k1.svg', './assets/imaj/n1.svg', './assets/imaj/p1.svg', './assets/imaj/q1.svg', './assets/imaj/r1.svg',
+  './assets/sound/move.mp3', './assets/sound/capture.mp3','./assets/sound/clap.mp3', './assets/sound/wrong.mp3', './assets/sound/check.mp3'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[SW] Tüm varlıklar (taşlar dahil) önbelleğe alınıyor...');
+      return cache.addAll(OFFLINE_DOSYALAR);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => {
+      return Promise.all(names.map((name) => {
+        if (name !== CACHE_NAME) return caches.delete(name);
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  // Dış API'leri ve Firebase'i pas geç
+  if (url.origin.includes('translated.net') || url.origin.includes('lingva.ml') || url.origin.includes('firebaseapp.com')) {
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
+
+//self.addEventListener('install', (event) => {
+//  event.waitUntil(
+//    caches.open(CACHE_NAME).then((cache) => {
+//      return Promise.all(
+//        OFFLINE_DOSYALAR.map(url => {
+//          return fetch(url).then(res => {
+//            if(!res.ok) throw new Error("Kayıp Dosya: " + url);
+//            return cache.add(url);
+//          }).catch(err => console.log("Hata:", err));
+//        })
+//      );
+//    })
+//  );
+//});
